6bee4aaec0a6c7bd75f325480c154407bfb343bd
Kirill Lazarev <lazarev@MBP-Irasa.local>
2011-07-27 19:47:10 +0400

    Add default path.

--- lib/number_to_words_ru.rb
+++ lib/number_to_words_ru.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 
 require 'i18n'
 require 'pluralization'
+$:.push File.join(File.dirname(__FILE__))
 I18n.load_path = ['locales/integer.ru.yml', 'locales/plurals.rb']
 I18n.locale = :ru
 
