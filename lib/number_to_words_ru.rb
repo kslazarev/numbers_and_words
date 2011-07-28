d17521115a760b0c39a36990f2f2b1b4574636b0
Kirill Lazarev <k.s.lazarev@gmail.com>
2011-07-28 16:24:46 +0400

    Change gem initialize in number_to_words_ru.

--- lib/number_to_words_ru.rb
+++ lib/number_to_words_ru.rb
@@ -1,14 +1,19 @@
 require 'rubygems'
-
 require 'i18n'
 require 'pluralization'
-
-I18n.load_path = [
-  Dir.pwd + '/lib/locales/integer.ru.yml',
-  Dir.pwd + '/lib/locales/plurals.rb'
-]
-I18n.locale = :ru
-
 require 'number_to_words_ru/integer'
 
+module NumberToWordsRu
+  extend self
 
+  def init_i18n
+    I18n.locale = :ru
+    I18n.load_path.unshift(*locale_files)
+  end
+
+  def locale_files
+    Dir[File.join(File.dirname(__FILE__), "locales", "**/*")]
+  end
+end
+
+NumberToWordsRu.init_i18n
