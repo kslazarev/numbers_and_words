2f9f824d3bea92be6889e9a5445c52e2e5323423
kslazarev <k.s.lazarev@gmail.com>
2012-10-15 03:27:07 +0400

    #32. Refactoring tests. Remove duplication code.

---
+++ spec/numbers_and_words/numbers_and_words_spec.rb
@@ -0,0 +1,16 @@
+# coding: utf-8
+require 'spec_helper'
+require 'shared_examples/correct_fixture_examples'
+
+[Integer, Array].each do |type|
+  describe type do
+    NumbersAndWords::I18n::Initialization.languages.each do |locale|
+      context locale do
+        around(:each) { |example| ::I18n.with_locale(locale) { example.run } }
+
+        subject { fixture_examples type, locale }
+        it_behaves_like 'correct fixture examples'
+      end
+    end
+  end
+end
--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,6 +1,6 @@
-def fixture_examples
+def fixture_examples type, language
   caller.first =~ /(.*):(\d+)/
   dir_name = File.dirname($1)
-  fixture_file = "#{File.basename($1).split('_spec').first}.yml"
-  YAML::load File.open([dir_name, 'fixture_examples', fixture_file].join('/'))
+  fixture_file = "#{language}.yml"
+  YAML::load File.open([dir_name, type, 'fixture_examples', fixture_file].join('/'))
 end
