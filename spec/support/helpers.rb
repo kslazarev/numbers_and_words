15aeee79f7fa77a4b63dc32421543b9e3a1ae21a
kslazarev <k.s.lazarev@gmail.com>
2012-10-15 03:40:42 +0400

    #32. Refactoring tests. Fix type name in fixture path.

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -2,5 +2,7 @@ def fixture_examples type, language
   caller.first =~ /(.*):(\d+)/
   dir_name = File.dirname($1)
   fixture_file = "#{language}.yml"
-  YAML::load File.open([dir_name, type, 'fixture_examples', fixture_file].join('/'))
+  YAML::load File.open(
+    [dir_name, type.inspect.underscore, 'fixture_examples', fixture_file].join('/')
+  )
 end
