745b2ccad24e6df07ae4d0a86edd6c42f61e7ad7
kslazarev <k.s.lazarev@gmail.com>
2012-10-18 21:19:47 +0400

    #33. Add Dutch language. (experimental)

---
+++ lib/numbers_and_words/translations_helpers/nl.rb
@@ -0,0 +1,18 @@
+module NumbersAndWords
+  module TranslationsHelpers
+    module Nl
+      include NumbersAndWords::TranslationsHelpers::Base
+      include NumbersAndWords::TranslationsHelpers::Families::Latin
+
+      private
+
+      def translation_tens_with_ones numbers
+        [ translation_ones(numbers[0]), translation_tens(numbers[1])].join 'en'
+      end
+
+      def translation_hundreds number
+        1 == number ? t(:hundreds) : [t(:ones)[number], t(:hundreds)].join
+      end
+    end
+  end
+end
