8ef1039b4a69ef4bea29046c5c75b56b0302ca71
kslazarev <k.s.lazarev@gmail.com>
2013-03-17 04:30:19 +0400

    #54. Support fractional form for Ukranian.

---
+++ lib/numbers_and_words/strategies/figures_converter/decorators/ua/base.rb
@@ -0,0 +1,12 @@
+module NumbersAndWords
+  module Strategies
+    module FiguresConverter
+      module Decorators
+        module Ua
+          class Base < Decorators::Base
+          end
+        end
+      end
+    end
+  end
+end
