9a803c3338f273511e6adc57afbebf487bad4752
Luca Pradovera <luca.pradovera@gmail.com>
2014-03-12 18:42:08 +0100

    More elegant parameters

--- lib/numbers_and_words/strategies/figures_converter/options/en/pronounced.rb
+++ lib/numbers_and_words/strategies/figures_converter/options/en/pronounced.rb
@@ -21,73 +21,55 @@ module NumbersAndWords
             end
 
             def process language, figures
-              @language = language
-              @figures = figures
-              units, tens, hundreds, thousands = *@figures.to_a.dup
-
               if figures.to_a.count > 4
-                @language.number_without_capacity_to_words + @language.complex_number_to_words
-              elsif @figures.capacity_count
-                handle_thousands thousands, hundreds, tens, units
-              elsif @figures.hundreds
-                handle_hundreds hundreds, tens, units
-              elsif @figures.tens or @figures.ones
-                @language.simple_number_to_words
+                language.number_without_capacity_to_words + language.complex_number_to_words
+              elsif figures.capacity_count
+                handle_thousands language, figures
+              elsif figures.hundreds
+                handle_hundreds language, figures
+              elsif figures.tens or figures.ones
+                language.simple_number_to_words
               else
                 []
               end
             end
 
-            def handle_thousands thousands, hundreds, tens, units
+            def handle_thousands language, figures
+              units, tens, hundreds, thousands = *figures.to_a.dup
               if hundreds == 0
-                @language.number_without_capacity_to_words + @language.complex_number_to_words
+                language.number_without_capacity_to_words + language.complex_number_to_words
               else
-                result = []
-                if tens == 0 && units == 0
-                  result.push 'hundred'
-                else
-                  result += tens_with_oh tens, units
-                end
+                result = tens_with_oh language, figures
                 result.push "#{thousands}#{hundreds}".to_i.to_words
                 result
               end
             end
 
-            def handle_hundreds hundreds, tens, units
-              result = []
-              if tens == 0
-                if units == 0
-                  result.push @language.hundreds_number_to_words
-                else
-                  result.push @language.ones
-                  result.push 'oh'
-                  result.push hundreds.to_words
-                end
-              else
-                if @figures.teens
-                  result.push @language.teens
-                elsif @figures.tens
-                  result.push @language.complex_tens
-                end
-                result.push hundreds.to_words
-              end
+            def handle_hundreds language, figures
+              units, tens, hundreds = *figures.to_a.dup
+              result = tens_with_oh language, figures
+              result.push hundreds.to_words
               result
             end
 
-            def tens_with_oh tens, units
+            def tens_with_oh language, figures
+              units, tens = *figures.to_a.dup
               result = []
               if tens == 0
-                if units != 0
-                  result.push @language.ones
+                if units == 0
+                  result.push 'hundred'
+                else
+                  result.push language.ones
                   result.push 'oh'
                 end
               else
-                if @figures.teens
-                  result.push @language.teens
-                elsif @figures.tens
-                  result.push @language.complex_tens
+                if figures.teens
+                  result.push language.teens
+                elsif figures.tens
+                  result.push language.complex_tens
                 end
               end
+              result
             end
           end
         end
