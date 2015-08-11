07e797e70ec4eac51e7ca2dd53e64e31f2b96d8c
Canbey Bilgili <cbilgili@gmail.com>
2015-08-11 11:00:48 +0300

    Turkish Language Support

--- lib/numbers_and_words/translations/tr.rb
+++ lib/numbers_and_words/translations/tr.rb
@@ -6,6 +6,15 @@ module NumbersAndWords
       def tens_with_ones numbers, options = {}
         super numbers, :separator => ' '
       end
+
+      def hundreds number, options = {}
+        if number == 1
+          [t([options[:prefix], :hundreds].join('.'))].join options[:separator] || ' '
+        else
+          [t(:ones)[number], t([options[:prefix], :hundreds].join('.'))].join options[:separator] || ' '
+        end
+      end
+
     end
   end
 end
