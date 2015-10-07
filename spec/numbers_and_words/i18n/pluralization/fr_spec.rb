4942985b00b972aeef39e23340c3ac80710ac845
joseramonc <joserracamacho@gmail.com>
2015-10-06 21:12:50 -0500

    use one/other convention
    fix #115

--- spec/numbers_and_words/i18n/pluralization/fr_spec.rb
+++ spec/numbers_and_words/i18n/pluralization/fr_spec.rb
@@ -3,22 +3,22 @@ require 'spec_helper'
 describe I18n, 'fr' do
   before(:each) do
     @hash = {}
-    %w(one many).each { |key| @hash[key.to_sym] = key }
+    %w(one other).each { |key| @hash[key.to_sym] = key }
     @backend = I18n.backend
   end
 
   it "should pluralize correctly" do
     @backend.send(:pluralize, :'fr', @hash, 1).should eq('one')
-    @backend.send(:pluralize, :'fr', @hash, 2).should eq('many')
-    @backend.send(:pluralize, :'fr', @hash, 3).should eq('many')
-    @backend.send(:pluralize, :'fr', @hash, 5).should eq('many')
-    @backend.send(:pluralize, :'fr', @hash, 10).should eq('many')
-    @backend.send(:pluralize, :'fr', @hash, 11).should eq('many')
+    @backend.send(:pluralize, :'fr', @hash, 2).should eq('other')
+    @backend.send(:pluralize, :'fr', @hash, 3).should eq('other')
+    @backend.send(:pluralize, :'fr', @hash, 5).should eq('other')
+    @backend.send(:pluralize, :'fr', @hash, 10).should eq('other')
+    @backend.send(:pluralize, :'fr', @hash, 11).should eq('other')
     @backend.send(:pluralize, :'fr', @hash, 21).should eq('one')
-    @backend.send(:pluralize, :'fr', @hash, 29).should eq('many')
+    @backend.send(:pluralize, :'fr', @hash, 29).should eq('other')
     @backend.send(:pluralize, :'fr', @hash, 131).should eq('one')
-    @backend.send(:pluralize, :'fr', @hash, 1.31).should eq('many')
-    @backend.send(:pluralize, :'fr', @hash, 2.31).should eq('many')
-    @backend.send(:pluralize, :'fr', @hash, 3.31).should eq('many')
+    @backend.send(:pluralize, :'fr', @hash, 1.31).should eq('other')
+    @backend.send(:pluralize, :'fr', @hash, 2.31).should eq('other')
+    @backend.send(:pluralize, :'fr', @hash, 3.31).should eq('other')
   end
 end
