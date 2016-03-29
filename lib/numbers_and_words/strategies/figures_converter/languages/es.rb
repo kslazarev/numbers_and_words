4612f1f079ec9a5e4c4c9c5b5b17fced2efd89d2
Jose Luis Duran <jlduran@users.noreply.github.com>
2016-03-29 14:26:33 -0300

    Fix ES typo twenty-one something
    veintiun -> veintiún

--- lib/numbers_and_words/strategies/figures_converter/languages/es.rb
+++ lib/numbers_and_words/strategies/figures_converter/languages/es.rb
@@ -11,33 +11,39 @@ module NumbersAndWords
             words.push megs(capacity_words) unless capacity_words.empty?
 
             if 0 < @current_capacity
-              # eg 1000000 should be "un millón" not "uno millón"
-              #    501000 should be "quinientos un mil" not "quinientos uno mil"
-              #    1000 should be "mil" not "uno mil"
-              # so we replace "uno" with "un" throughout or delete it if that is the case
-
-              capacity_words = capacity_words.map { |word|
-                word.gsub(@translations.ones(1), @translations.one) unless is_a_thousand? and is_a_one?
-              }.compact
+              capacity_words = capacity_words.map do |word|
+                if twenty_one?
+                  word.gsub(@translations.ones(1), @translations.one_twenties)
+                elsif !thousand? || !one?
+                  word.gsub(@translations.ones(1), @translations.one)
+                end
+              end.compact
             end
 
             words + capacity_words
           end
 
-          def is_a_one?
+          def hundreds
+            super({ is_hundred: (figures[1, 2] == [0, 1] &&
+                                 simple_number_to_words.empty?) })
+          end
+
+          def megs(capacity_words)
+            super({ is_one: capacity_words == [@translations.ones(1)] })
+          end
+
+          private
+
+          def one?
             [translations.ones(1)] == words_in_capacity(@current_capacity)
           end
 
-          def is_a_thousand?
+          def thousand?
             @current_capacity.odd?
           end
 
-          def hundreds
-            super({:is_hundred => (figures[1,2] == [0,1] && simple_number_to_words.empty?)})
-          end
-
-          def megs capacity_words
-            super({:is_one => capacity_words == [@translations.ones(1)]})
+          def twenty_one?
+            figures.reverse[0..1] == [2, 1]
           end
         end
       end
