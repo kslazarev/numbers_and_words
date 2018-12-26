1c5d63f854869bdea97b8e3a049c09fd5cb7207a
Jose Luis Duran <jlduran@users.noreply.github.com>
2018-12-25 23:01:20 -0200

    Update to Ruby 2.6

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -4,7 +4,10 @@ def fixture_examples(type, language)
   caller(1..1).first =~ /(.*):(\d+)/
   dir_name = File.dirname(Regexp.last_match(1))
   fixture_file = "#{language}.yml"
-  YAML.safe_load(File.open(
-                   [dir_name, type.inspect.downcase, 'fixture_examples', fixture_file].join('/')
-                 ), [Symbol])
+  file = File.open([dir_name, type.inspect.downcase, 'fixture_examples', fixture_file].join('/'))
+  if Gem::Version.new(Psych::VERSION) >= Gem::Version.new('3.1.0.pre1')
+    YAML.safe_load(file, permitted_classes: [Symbol])
+  else
+    YAML.safe_load(file, [Symbol])
+  end
 end
