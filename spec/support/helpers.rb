030c485c0276521c771795ac014e0a3c99591f0d
Jose Luis Duran <jlduran@users.noreply.github.com>
2020-05-01 22:57:23 -0300

    Remove psych workaround (#174)

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -5,9 +5,6 @@ def fixture_examples(type, language)
   dir_name = File.dirname(Regexp.last_match(1))
   fixture_file = "#{language}.yml"
   file = File.open([dir_name, type.inspect.downcase, 'fixture_examples', fixture_file].join('/'))
-  if Gem::Version.new(Psych::VERSION) >= Gem::Version.new('3.1.0.pre1')
-    YAML.safe_load(file, permitted_classes: [Symbol])
-  else
-    YAML.safe_load(file, [Symbol])
-  end
+
+  YAML.safe_load(file, permitted_classes: [Symbol])
 end
