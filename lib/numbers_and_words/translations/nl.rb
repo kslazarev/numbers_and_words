86f8bfc0b4c8a8889fba1907cb27dabca985d96b
Jose Luis Duran <jlduran@users.noreply.github.com>
2021-06-25 14:08:43 -0300

    WIP: Is this OK?
    See: https://github.com/kslazarev/numbers_and_words/pull/182#issuecomment-868708117

--- lib/numbers_and_words/translations/nl.rb
+++ lib/numbers_and_words/translations/nl.rb
@@ -6,13 +6,21 @@ module NumbersAndWords
       include NumbersAndWords::Translations::Families::Latin
 
       def tens_with_ones(numbers, _options = {})
-        [ones(numbers[0]), tens(numbers[1])].join(union)
+        [ones(numbers[0]), tens(numbers[1])].join(union(numbers[0]))
       end
 
       def hundreds(number, _options = {})
         return t(:hundreds) if number == 1
 
         super number, separator: ''
+      end
+
+      private
+
+      def union(unit)
+        return t :union23 if [2, 3].include?(unit)
+
+        t :union
       end
     end
   end
