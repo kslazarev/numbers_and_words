44d4993cfdce12cf3059f05e721e77d8a59235f8
Natalia Korosteleva <n.korosteleva@latera.ru>
2021-10-22 16:03:03 +0300

    HOPER-4840v0.10.5 Add az to 0.10.5

---
+++ lib/numbers_and_words/translations/az.rb
@@ -0,0 +1,20 @@
+module NumbersAndWords
+  module Translations
+    class Az < Base
+      include NumbersAndWords::Translations::Families::Latin
+
+      def tens_with_ones numbers, options = {}
+        super numbers, :separator => ' '
+      end
+
+      def hundreds number, options = {}
+        if number == 1
+          [t([options[:prefix], :hundreds].join('.'))].join options[:separator] || ' '
+        else
+          [t(:ones)[number], t([options[:prefix], :hundreds].join('.'))].join options[:separator] || ' '
+        end
+      end
+
+    end
+  end
+end
