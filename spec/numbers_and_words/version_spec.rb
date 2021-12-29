fd3099bfc26145d40062ad4517de4d247360da29
Jose Luis Duran <jlduran@users.noreply.github.com>
2021-12-28 22:28:05 -0300

    RuboCop: Re-enable Performance/Sum
    RuboCop has been fixed.  Re-enable Performance/Sum cop.
    
    No functional change intended.

--- spec/numbers_and_words/version_spec.rb
+++ spec/numbers_and_words/version_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 describe NumbersAndWords::VERSION do
   let(:version_array) { subject.split '.' }
-  let(:version_sum) { version_array.map(&:to_i).reduce :+ }
+  let(:version_sum) { version_array.sum(&:to_i) }
 
   it { expect(version_array).not_to be_empty }
   it { expect(version_array.size).to eq 3 }
