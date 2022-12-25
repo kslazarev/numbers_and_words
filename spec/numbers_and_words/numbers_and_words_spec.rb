dbddc43293b3ce7990bde37b67a032bbdec06890
Jose Luis Duran <jlduran@users.noreply.github.com>
2022-12-25 14:23:23 -0300

    RuboCop: Style/RedundantConstantBase: Remove redundant ::

--- spec/numbers_and_words/numbers_and_words_spec.rb
+++ spec/numbers_and_words/numbers_and_words_spec.rb
@@ -7,7 +7,7 @@ require 'shared_examples/correct_fixture_examples'
   describe type do
     NumbersAndWords::I18n.languages.each do |locale|
       context locale do
-        around { |example| ::I18n.with_locale(locale) { example.run } }
+        around { |example| I18n.with_locale(locale) { example.run } }
 
         it_behaves_like 'correct fixture examples', fixture_examples(type, locale)
       end
@@ -20,7 +20,7 @@ FLOAT_CAPABLE_LANGUAGES = %i[cs en-GB en es fr hu pt-BR ru ua vi].freeze
   describe type do
     FLOAT_CAPABLE_LANGUAGES.each do |locale|
       context locale do
-        around { |example| ::I18n.with_locale(locale) { example.run } }
+        around { |example| I18n.with_locale(locale) { example.run } }
 
         it_behaves_like 'correct fixture examples', fixture_examples(type, locale)
       end
